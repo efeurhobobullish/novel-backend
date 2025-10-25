5a8d983df59263f43ece90886e1061defb5803fe
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-10-25 07:42:27 -1100

    Update walletRoutes.js

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const { initiateRecharge, paystackWebhook, getWallet } = require("../controllers/walletController");
+const { fakeRecharge, getWallet } = require("../controllers/walletController");
 const auth = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.post("/recharge", auth, initiateRecharge);
+router.post("/recharge", auth, fakeRecharge);
 router.get("/", auth, getWallet);
-router.post("/webhook", paystackWebhook);
 
 module.exports = router;
