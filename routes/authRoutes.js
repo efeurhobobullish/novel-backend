568d291685e3355635ac1e1f55d147397b1d5010
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-10-25 12:31:10 -1100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { register, login } = require("../controllers/authController");
+const { register, login, getDashboard } = require("../controllers/authController");
+const auth = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/dashboard", auth, getDashboard);
 
 module.exports = router;
