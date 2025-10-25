1e14cdf5d091d462454b53af1222a50a1cf8a491
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-10-25 12:26:53 -1100

    Delete routes/novelRoutes.js

--- routes/novelRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getNovels } = require("../controllers/novelController");
-const router = express.Router();
-
-router.get("/", getNovels);
-
-module.exports = router;
