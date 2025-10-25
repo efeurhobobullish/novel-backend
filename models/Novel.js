8c9f544cbbcfe29822d3736dc4484d816080f3cb
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-10-25 12:50:49 -1100

    Update Novel.js

--- models/Novel.js
+++ models/Novel.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const novelSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  cover: String,
-  genre: String,
-  description: String,
-  episodes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Episode" }],
-  views: { type: Number, default: 0 }
-}, { timestamps: true });
+const novelSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    genre: { type: String },
+    cover: { type: String }, // image url
+    description: { type: String },
+    episodes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Episode" }],
+    views: { type: Number, default: 0 }
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Novel", novelSchema);
