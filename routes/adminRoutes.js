1b9885b541c82c6d317594f660329d611065ba28
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-10-25 12:26:03 -1100

    Delete routes/adminRoutes.js

--- routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createNovel, addEpisode } = require("../controllers/adminController");
-const auth = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.post("/novel", auth, createNovel);
-router.post("/episode", auth, addEpisode);
-
-module.exports = router;
