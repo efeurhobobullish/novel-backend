049a98da371d4f6e90173c23bcea75f00e85bfb7
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-10-25 06:25:59 -1100

    Create User model with schema and export

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["admin", "reader"], default: "reader" },
+  coins: { type: Number, default: 0 },
+  purchasedEpisodes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Episode" }]
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
