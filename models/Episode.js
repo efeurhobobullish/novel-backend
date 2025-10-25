f6737017f2bfcb0e71ac7f7fcc172af848a1f26c
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-10-25 06:26:43 -1100

    Create Episode model with schema definition

---
+++ models/Episode.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const episodeSchema = new mongoose.Schema({
+  novelId: { type: mongoose.Schema.Types.ObjectId, ref: "Novel", required: true },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  isFree: { type: Boolean, default: false },
+  price: { type: Number, default: 0 }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Episode", episodeSchema);
