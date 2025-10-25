0ea9a9ea645f86da4cdbad4772243fac2c81f6a6
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-10-25 12:26:36 -1100

    Delete routes/walletRoutes.js

--- routes/walletRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { fakeRecharge, getWallet } = require("../controllers/walletController");
-const auth = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.post("/recharge", auth, fakeRecharge);
-router.get("/", auth, getWallet);
-
-module.exports = router;
